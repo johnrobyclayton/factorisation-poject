56f63b5071f7bce66f0ec79e2887031488d9814f
johnrobyclayton <30948898+johnrobyclayton@users.noreply.github.com>
2023-05-19 18:08:47 +1000

    2023-05-19 from home work laptop

--- ExtendedEuclidianAlgorythm.py
+++ ExtendedEuclidianAlgorythm.py
@@ -14,42 +14,47 @@ def GCD(a, b):
 #B=-c
 #C=d-b
 #A*x+B*x=C
-a=3
-b=1
-c=5
-d=2
-print(a,"* x +",b,"=",c,"* y +",d)
-A=a
-B=-c
-C=d-b
+def diophantine(a,b,c,d):
+  #a*x+b=c*y+d
+  '''
+  a=3
+  b=1
+  c=5
+  d=2
+  '''
+  print(a,"* x +",b,"=",c,"* y +",d)
+  A=a
+  B=-c
+  C=d-b
 
 
-'''
-#Solve: 3x + 6y = 9 
-#x
-a = 3
-#b
-b = -5
-#c
-c = 2
-'''
-#Step 1 
-if A==0 and B==0:
-  if C == 0: 
-    print("Infinite Solutions are possible")
+  '''
+  #Solve: 3x + 6y = 9 
+  #x
+  a = 3
+  #b
+  b = -5
+  #c
+  c = 2
+  '''
+  #Step 1 
+  if A==0 and B==0:
+    if C == 0: 
+      return(-1,0,0)#print("Infinite Solutions are possible")
+    else:
+      return(-2,0,0)#print("Solution not possible")
+
+  #Step 2 
+  gcd, x1, y1 = GCD(A,B)
+
+  #Step 3 and 4 
+  if (C % gcd == 0):
+    x = x1 * (C//gcd)
+    y = y1 * (C//gcd)
+    print("The values of x and y are: ", x, ",", y)
+    print(a,'*(',c,'* x +',x,")+",b,"=",c,"* (",a,"* y +",y,")+",d)
+    return(0,x,y)
   else:
-    print("Solution not possible")
-
-#Step 2 
-gcd, x1, y1 = GCD(A,B)
-
-#Step 3 and 4 
-if (C % gcd == 0):
-  x = x1 * (C//gcd)
-  y = y1 * (C//gcd)
-  print("The values of x and y are: ", x, ",", y)
-  print(a,'*(',c,'* x +',x,")+",b,"=",c,"* (",a,"* y +",y,")+",d)
-else:
-  print("Solution not possible") 
- 
+    return(-2,0,0)#print("Solution not possible") 
+print(diophantine(30,17,42,11))  
     
--- modsearch.py
+++ modsearch.py
@@ -13,8 +13,8 @@ def trans(x):
 pp=pprint.PrettyPrinter(indent=2)
 
 moddict=dict()        
-for i in (30,42,60,70,72):
-    for j in range(2,i):
+for i in (2*3*5,2*3*7):
+    for j in range(1,i):
         moddict[(i,j)]=set()
         k=1
         #print('divisor ',i,' modulus ',j)
@@ -36,14 +36,18 @@ for i in (30,42,60,70,72):
                     if(scheme[pos]):
                         partitionproduct*=primelist[pos]
                 if(partitionproduct<=i and candidate/partitionproduct<=i and p):
-                    moddict[(i,j)].add(partitionproduct)
+                    #moddict[(i,j)].add((partitionproduct*candidate//partitionproduct,partitionproduct,candidate//partitionproduct))
+                    moddict[(i,j)].add((partitionproduct))
                     moddict[(i,j)].add(candidate//partitionproduct)
             k+=1
 
-
+'''
 delete = [key for key in moddict if len(moddict[key])>key[0]/4]
 for key in delete:
     del moddict[key]
-
-pp.pprint(moddict)                 
+'''
+#for key in ((2*3*5,144871%2*3*5),(2*3*7,144871%2*3*7)):
+#    print(key,moddict[key])     
+print(sorted(moddict[(30,144871%30)]))                
+print(sorted(moddict[(42,144871%42)]))                
         
