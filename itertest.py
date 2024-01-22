4b8663d972405fe5ae940eafe6234a1aa5c96fa3
johnrobyclayton <claytonjohnroby@techie.com>
2024-01-22 16:44:46 +1100

    floatmod

--- itertest.py
+++ itertest.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 def primegen():
     listofprimes=list()
@@ -47,6 +48,15 @@ def GCD(a, b):
     y1 = s 
     x1 = t - (b//a) * s
     return gcd, x1, y1
+
+def floatGCD(a, b):
+    if a == 0: 
+        return b, 0, 1
+    gcd, s, t = floatGCD(math.fmod(b,a), a)
+    y1 = s 
+    x1 = t - ((b-math.fmod(b,a))/a) * s
+    return gcd, x1, y1
+
 
 def diophantine(a,b,c,d):
   #a*x+b=c*y+d
@@ -97,6 +107,58 @@ def diophantine(a,b,c,d):
   else:
     return(-2,0,0)#print("Solution not possible") 
 
+
+def floatdiophantine(a,b,c,d):
+  #a*x+b=c*y+d
+  '''
+  a=3
+  b=1
+  c=5
+  d=2
+  '''
+  #print(a,"* x +",b,"=",c,"* y +",d)
+  A=a
+  B=-c
+  C=d-b
+  '''
+  #Solve: 3x + 6y = 9 
+  #x
+  a = 3
+  #b
+  b = -5
+  #c
+  c = 2
+  '''
+  #Step 1
+  #print(A,B) 
+  if A==0 and B==0:
+    if C == 0: 
+      return(-1,0,0)#print("Infinite Solutions are possible")
+    else:
+      return(-2,0,0)#print("Solution not possible")
+  #Step 2 
+  gcd, x1, y1 = floatGCD(A,B)
+  #Step 3 and 4 
+  if (C % gcd == 0):
+    x = x1 * ((C-math.fmod(C,gcd))/gcd)
+    y = y1 * ((C-math.fmod(C,gcd))/gcd)
+    while(x<0):
+        x+=c
+    while(x>c):
+        x-=c    
+    y = y1 * ((C-math.fmod(C,gcd))/gcd)
+    while(y<0):
+        y+=a
+    while(y>a):
+        y-=a
+    #print("The values of x and y are: ", x, ",", y)
+    #print(a,'*(',c,'* x +',x,")+",b,"=",c,"* (",a,"* y +",y,")+",d)
+    return(0,x,y)
+  else:
+    return(-2,0,0)#print("Solution not possible") 
+
+
+
 def generate_grid3(grid1, grid2):
     grid=dict()
     gridsize=grid1[0]*grid2[0]
@@ -121,7 +183,7 @@ def moddiffgen(prime,mod):
     if mod==0:
         #print('mod is zero, moddiff of a factor impossible')
         yield((1,(0,)))
-    pp=set(range(1,prime))
+    pp=set(range(1,int(prime)))
     diffs=set()
     while len(pp):
         #print(pp)
@@ -144,10 +206,38 @@ def moddiffgen(prime,mod):
             if found:
                 break
 
+def floatmoddiffgen(prime,mod):
+    if mod==0:
+        #print('mod is zero, moddiff of a factor impossible')
+        yield((1,(0,)))
+    pp=set(range(1,int(prime)))
+    diffs=set()
+    while len(pp):
+        #print(pp)
+        found=False
+        for i in pp:
+            for j in pp:
+                if  math.fmod((i*j),prime)==mod:
+                    if (math.fmod((prime+i-j),prime)) not in diffs:
+                        #print(i,j,i*j,(i*j)%prime,prime,mod,((prime+i-j)%prime))
+                        diffs.add(math.fmod((prime+i-j),prime))
+                        diffs.add(math.fmod((prime-i+j),prime))
+                        yield(prime,math.fmod((prime+i-j),prime))
+                        if (math.fmod((prime+i-j),prime))!=0:
+                            yield(prime,math.fmod((prime-i+j),prime))
+                    pp.discard(i)
+                    pp.discard(j)
+                    found=True
+                if found:
+                    break
+            if found:
+                break
 
-def gridgen(factorlist1, factorlist2):
+
+def gridgen(factorlist1, factorlist2,maxval):
     islist1=True
     islist2=True
+    '''
     try:
         factor1=next(factorlist1)
     except StopIteration:
@@ -156,14 +246,37 @@ def gridgen(factorlist1, factorlist2):
         factor2=next(factorlist2)
     except StopIteration:
         islist2=False
-    factor2=next(factorlist2)
-    gridsize=factor1[0]*factor2[0]
-    A1=factor1[0]*diophantine(factor1[0],0,factor2[0],1)[1]
-    A2=factor2[0]*diophantine(factor1[0],1,factor2[0],0)[2]
+    '''
+    #factor2=next(factorlist2)
+    gridsize=factorlist1[0][0]*factorlist2[0][0]
+    A1=factorlist1[0][0]*diophantine(factorlist1[0][0],0,factorlist2[0][0],1)[1]
+    A2=factorlist2[0][0]*diophantine(factorlist1[0][0],1,factorlist2[0][0],0)[2]
     for a1 in factorlist1:
         for a2 in factorlist2:
-            yield((gridsize,(A1*a2[1]+A2*a1[1])%gridsize))
+            if((A1*a2[1]+A2*a1[1])%gridsize<maxval):
+                yield((gridsize,(A1*a2[1]+A2*a1[1])%gridsize))
    
+def floatgridgen(factorlist1, factorlist2,maxval):
+    islist1=True
+    islist2=True
+    '''
+    try:
+        factor1=next(factorlist1)
+    except StopIteration:
+        islist1=False
+    try:
+        factor2=next(factorlist2)
+    except StopIteration:
+        islist2=False
+    '''
+    #factor2=next(factorlist2)
+    gridsize=factorlist1[0][0]*factorlist2[0][0]
+    A1=factorlist1[0][0]*floatdiophantine(factorlist1[0][0],0,factorlist2[0][0],1)[1]
+    A2=factorlist2[0][0]*floatdiophantine(factorlist1[0][0],1,factorlist2[0][0],0)[2]
+    for a1 in factorlist1:
+        for a2 in factorlist2:
+            if(math.fmod((A1*a2[1]+A2*a1[1]),gridsize)<maxval):
+                yield((gridsize,math.fmod((A1*a2[1]+A2*a1[1]),gridsize)))
 
 
 
@@ -177,16 +290,36 @@ def chunked_iterable(iterable, size):
         yield chunk
 
 #primegenerator=primegen()
-moddiffgenerator=moddiffgen(gridgen(11,5),gridgen(13,6))
-gridgenerator1=gridgen(11,5)
-gridgenerator2=gridgen(13,6)
-moddiffgenerator=moddiffgen(next(gridgenerator1),next(gridgenerator2))
+maxval=100
+p=163
+q=323
+d=p*q
+#moddiffgenerator=moddiffgen(gridgen(11,5),gridgen(13,6))
+floatmoddiffgenerator2=moddiffgen(2.0,d%2.0)
+floatmoddiffgenerator3=moddiffgen(3.0,d%3.0)
+'''
+gridgenerator6=gridgen(list(moddiffgenerator2),list(moddiffgenerator3),d**.5*1.2)
+moddiffgenerator5=moddiffgen(5.0,d%5.0)
+gridgenerator30=gridgen(list(gridgenerator6),list(moddiffgenerator5),d**.5*1.2)
+moddiffgenerator7=moddiffgen(7.0,d%7.0)
+gridgenerator210=gridgen(list(gridgenerator30),list(moddiffgenerator7),d**.5*1.2)
+moddiffgenerator11=moddiffgen(11.0,d%11.0)
+gridgenerator2310=gridgen(list(gridgenerator210),list(moddiffgenerator11),d**.5*1.2)
+moddiffgenerator13=moddiffgen(13.0,d%13.0)
+gridgenerator30030=gridgen(list(gridgenerator2310),list(moddiffgenerator13),d**.5*1.2)
+moddiffgenerator17=moddiffgen(17.0,d%17.0)
+gridgenerator510510=gridgen(list(gridgenerator30030),list(moddiffgenerator17),d**.5*1.2)
+'''
+#moddiffgenerator=moddiffgen(next(gridgenerator1),next(gridgenerator2))
 #primeproductgenerator=primeproductuptogen(1000000)
 for index in range(0,50,1):
     #print('prime',next(primegenerator))
     try:
-        
-        print('moddiff',next(moddiffgenerator))
+        print(next(floatmoddiffgenerator3))
+        #print(next(gridgenerator6))
+        #print(next(gridgenerator510510),d**.5*1.2)
+        #print(next(gridgenerator1))
+        #print('moddiff',next(moddiffgenerator))
     #    print('primeprod',next(primeproductgenerator))
     except StopIteration:
         pass
